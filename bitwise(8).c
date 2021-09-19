c20bc40f9e4d2db1af58eaf97ba3eca0e35ce689
hameedshaik555 <88450801+hameedshaik555@users.noreply.github.com>
2021-09-19 20:01:13 +0530

    Create bitwise(8).c

---
+++ bitwise(8).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define INT_SIZE sizeof(int) * 8 /* Integer size in bits */
+
+int main()
+{
+    int num, order, i;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+
+    order = INT_SIZE - 1;
+
+
+    for(i=0; i<INT_SIZE; i++)
+    {
+        
+        if((num>>i) & 1)
+        {
+            order = i;
+
+            
+            break;
+        }
+    }
+
+    printf("Lowest order set bit in %d is %d", num, order);
+
+    return 0;
+}
